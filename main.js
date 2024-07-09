f7ec163cea67d483b607a751fd47494fbd59e56d
Sungmin-Kim-Dev <dreamerk24dev@gmail.com>
2024-07-09 19:46:59 +0900

    API redirect

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let news = [];
 
 const getLatestNews = async () => {
   // const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
-  const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`);
+  const url = new URL(`https://frabjous-medovik-94ad71.netlify.app/top-headlines`);
   console.log('url:', url);
   const response = await fetch(url);
   const data = await response.json();
