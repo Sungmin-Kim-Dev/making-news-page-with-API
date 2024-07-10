c6c65b6f10a9907d4e0b394a5c2a61deeb6af6ab
Sungmin-Kim-Dev <dreamerk24dev@gmail.com>
2024-07-11 02:35:11 +0900

    HTML page

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 const API_KEY = `8ca041837c814015be2990bcade21399`;
 let news = [];
+const searchBtn = document.querySelector('.fa-magnifying-glass');
+
+searchBtn.addEventListener('click', searchInput);
+
+function searchInput() {
+  document.getElementById('input-area').classList.toggle('d-none');
+}
+function openSide() {
+  document.getElementById("side-menu").style.width = "250px";
+}
+function closeSide() {
+  document.getElementById("side-menu").style.width = "0px";
+}
 
 const getLatestNews = async () => {
   // const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
-  const url = new URL(`https://frabjous-medovik-94ad71.netlify.app/top-headlines`);
+  // const url = new URL(`https://frabjous-medovik-94ad71.netlify.app/top-headlines`);
   console.log('url:', url);
   const response = await fetch(url);
   const data = await response.json();
@@ -11,7 +24,3 @@ const getLatestNews = async () => {
   console.log('news:', news);
 };
 getLatestNews();
-
-for (let i = 0; i < 5; i++) {
-  console.log('after:', i);
-}
